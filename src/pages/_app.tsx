5c5fe26b6e87c96241a37b96fa5264ab9fab61ce
kelvi <kelvi_ugo@hotmail.com>
2022-04-13 17:59:32 -0300

    feat: cria global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,23 @@
 import type { AppProps } from 'next/app';
+import { GlobalStyles } from 'styles/global';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-192.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
