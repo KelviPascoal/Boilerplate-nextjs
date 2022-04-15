d34db8563bf7deadcfeafcb77277923f04bd1f42
kelvi <kelvi_ugo@hotmail.com>
2022-04-15 16:50:37 -0300

    feat: instala next-pwa e configura

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPwa = require('next-pwa')
+
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = nextConfig
+module.exports = withPwa({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import { Main } from 'components/Main';
 
-const bla = 'asdasdas';
 const Home: NextPage = () => {
   return <Main />;
 };
