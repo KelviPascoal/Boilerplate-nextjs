6ed10cc748dc93c336711828bd412e33737bbc5d
kelvi <kelvi_ugo@hotmail.com>
2022-04-14 23:02:33 -0300

    fix: corrige jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    moduleNameMapper: {
-    '^styled-components': '<rooDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
-    },
     testEnvironment: 'jsdom',
     testPathIgnorePatterns: ['/node_modules/', '/.next/'],
     collectCoverage: true,
