bac999963d50ec0ec34f651137ac5fa67ea1dbcc
kelvi <kelvi_ugo@hotmail.com>
2022-04-16 19:15:17 -0300

    chore: retira babel e configura nextConfig para compilar o styled-components

--- next.config.js
+++ next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPwa = require('next-pwa')
-
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
 module.exports = withPwa({
+  reactStrictMode: true,
+
   pwa: {
     dest: 'public',
     disable: !isProd
-  }
+  },
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+
 })
